4556124d635729177cb250cfda28d83c12d6b845
Sukanta Purkayastha <spurkayastha83@gmail.com>
2022-12-19 11:44:06 +0600

    Change Master page title

--- resources/views/backend/layouts/head.blade.php
+++ resources/views/backend/layouts/head.blade.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Online Musical Shop || DASHBOARD</title>
+    <title>E-Mart</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{asset('backend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
